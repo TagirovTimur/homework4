582e47e3b5583060a5db7ed42ce38aac7315cc61
TagirovTimur <Attteupoh@gmail.com>
2024-07-27 14:13:14 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input()
+full = len(my_string)
+print(full)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
